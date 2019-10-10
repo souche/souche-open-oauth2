54f032d344c1919c0f06e376b641c5919ab3e995
plusmancn <plusmancn@gmail.com>
2019-10-10 14:38:09 +0800

    chore: initial commit

---
+++ src/main/java/com/souche/open/soucheopenoauth2example/SoucheOpenOauth2ExampleApplication.java
@@ -0,0 +1,13 @@
+package com.souche.open.soucheopenoauth2example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoucheOpenOauth2ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoucheOpenOauth2ExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/souche/open/soucheopenoauth2example/SoucheOpenOauth2ExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.souche.open.soucheopenoauth2example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SoucheOpenOauth2ExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
